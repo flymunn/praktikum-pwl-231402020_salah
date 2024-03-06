9ae3df8e84723cb44c66e925c22125452e0af2e6
flymunn <mwddh06@gmail.com>
2024-03-06 16:20:14 +0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Food List</title>
+</head>
+<body>
+
+    <h2>Food List</h2>
+
+    <ul>
+        <li>Apple</li>
+        <li>Banana</li>
+        <li>Carrot</li>
+        <li>Chicken</li>
+        <li>Chocolate</li>
+        <li>Coffee</li>
+        <li>Cookie</li>
+        <li>Fish</li>
+        <li>Grapes</li>
+        <li>Ice Cream</li>
+    </ul>
+
+</body>
+</html>
